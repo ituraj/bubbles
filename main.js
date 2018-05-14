cd06502788af13d9fc80cf660736c094c266bf1d
Izabela <Izabela@overmind-the3rd.kea.local>
2018-05-14 12:00:58 +0200

    main.js add

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
-"use strict";
+`use strict`;
 
-const divs = document.querySelectorAll("#app div");
+const divs = document.querySelectorAll(`#app div`);
 
 divs.forEach(function(div){
-    div.style.transform = translate(50vw, 50vh);
+    div.addEventListener(`mouseenter`, function(){
+        div.style.transform = `translate(${Math.random()*100}vw, ${Math.random()*100}vh)`;
+        div.style.backgroundColor = `hsl(${Math.random()*360}, ${Math.random()*100}%, 50%)`;
+    })
 })
